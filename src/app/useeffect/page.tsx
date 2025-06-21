4fd20b4e4a18d49d7a3fe1eab97745da274f8d07
Rounak00 <mukherjeerounak10@gmail.com>
2025-06-21 22:33:32 +0530

    Machine coding in react started

---
+++ src/app/useeffect/page.tsx
@@ -0,0 +1,10 @@
+import UseEffect from '@/views/useeffect'
+import React from 'react'
+
+const useEffectPage = () => {
+  return (
+    <div><UseEffect/></div>
+  )
+}
+
+export default useEffectPage
