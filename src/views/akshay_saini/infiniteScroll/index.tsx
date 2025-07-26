7e4936e574b3bc1b64028ac2babf46fd1536c116
Rounak00 <mukherjeerounak10@gmail.com>
2025-07-26 11:28:31 +0530

    akshay saini LLD working on progress

---
+++ src/views/akshay_saini/infiniteScroll/index.tsx
@@ -0,0 +1,9 @@
+/*
+only three rules are here for that,
+
+1. windows.scrollY() -> returns how much scroll in the document 
+2. windows.innerHeight() -> height of windows
+3. document.body.scrollY() -> height of ducment 
+
+Formula -> 1+2 >= 3 then call the api
+*/
